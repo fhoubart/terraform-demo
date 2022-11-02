bf27fa4f9386eb7d3b451c12314e3e179e47a0a1
HOUBART FLORENT <florent.houbart@gmail.com>
2022-11-02 23:08:44 +0100

    network

--- network/main.tf
+++ network/main.tf
@@ -41,5 +41,5 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table
+  route_table_id = aws_route_table.public_route_table.id
 }
