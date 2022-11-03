d879c795ba9a3f8b9635efcb4130a243f053d4ac
HOUBART FLORENT <florent.houbart@gmail.com>
2022-11-03 14:49:06 +0100

    network

--- network/main.tf
+++ network/main.tf
@@ -44,3 +44,25 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_network_interface" "networkinterface" {
+  subnet_id   = aws_subnet.public_subnet.id
+  //private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0ff8a91507f77f867"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  network_interface {
+    network_interface_id = aws_network_interface.networkinterface.id
+    device_index         = 0
+  }
+  tags = {
+    Name = "Web"
+  }
+}
