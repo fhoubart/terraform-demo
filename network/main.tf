d0e6cd244cc59471d642f94773e3b7c4cb77c546
HOUBART FLORENT <florent.houbart@gmail.com>
2022-11-04 10:16:04 +0100

    network

--- network/main.tf
+++ network/main.tf
@@ -117,6 +117,15 @@ resource "aws_security_group" "allow_web" {
     //ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   tags = {
     Name = "allow_tls"
   }
