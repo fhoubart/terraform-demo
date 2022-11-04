92f26708ef89e1aafdb864e9e3697aeed568eec4
HOUBART FLORENT <florent.houbart@gmail.com>
2022-11-04 10:25:47 +0100

    network

--- network/main.tf
+++ network/main.tf
@@ -68,6 +68,7 @@ resource "aws_instance" "web" {
   subnet_id = aws_subnet.public_subnet.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_web.id]
+  key_name = "keydemo"
 /*  user_data = <<EOF
 #!/bin/sh
 echo "Starting user data script"
